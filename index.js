f7b2ad8a6a8d3d668470c9353f20a19fdd621205
AJvM5874 <67021280+AJvM5874@users.noreply.github.com>
2021-05-29 11:08:46 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const age = 16;
+
+
+if (age >= 18) {
+
+    console.log("Je bent oud genoeg voor toegang.")
+    
+}else {
+    console.log("Je bent te jong voor toegang.")
+}
+
+const isFemale = true;
+
+if (isFemale){
+    console.log("Je hebt toegang voor Ladiesnight")
+}else{ 
+    console.log("Je hebt geen toegang tot Ladiesnight")
+}
+
+const driverStatus = "bob";
+
+if (driverStatus == "bob"){
+    console.log("Je bent de bob dus je mag rijden.")
+}else{
+    console.log("Je bent niet de bob dus je mag niet rijden.")
+}
